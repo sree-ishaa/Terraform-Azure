cecdbd3ead3b92b9f0c3c1c1a0af4c23de2c6dcf
sree-ishaa <61211023+sree-ishaa@users.noreply.github.com>
2021-08-31 14:55:40 +0100

    Create vnet-subnet.tf

---
+++ Win-server/vnet-subnet.tf
@@ -0,0 +1,31 @@
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.rg1.name}"
+
+    tags = {
+        environment = "Virtual Network 1"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "${azurerm_resource_group.rg1.name}"
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "eastus"
+    resource_group_name          = "${azurerm_resource_group.rg1.name}"
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
